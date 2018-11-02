d853435638562c861e7b629e2cdb41f9c1ab77a9
vincentleandr <31660411+vincentleandr@users.noreply.github.com>
2018-11-02 10:54:55 +0700

    swapi dashboard ongoing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/style.min.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor(props) {
+        super(props);
+
+        this.state = {
+			starships: []
+		}
+		
+		this.changeName = this.changeName.bind(this);
+	}
+
+	render() {
+		return (
+			<div>
+				<header>
+					<h1>SWAPI</h1>
+				</header>
+				<div class="content">
+					<h2>List of Starships</h2>
+
+
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
